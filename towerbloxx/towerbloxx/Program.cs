705777f45f20d9f2963ca042c0c690b65e6b4776
terp1s <90653457+terp1s@users.noreply.github.com>
2023-10-03 16:46:30 +0200

    Add files via upload

---
+++ towerbloxx/towerbloxx/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace towerbloxx
+{
+    class Tabulka
+    {
+        
+        int[,] tabulka;
+
+        public void doplnovani(int n, int k)
+        {
+            tabulka = new int[n, k];
+
+            for (int i = 0; i <= k; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    
+                    if (k == 1)
+                    {
+                        tabulka[n,k] = n;
+                    }
+                    else
+                    {
+                        velkaMatematika(n,k);
+                    }
+                }
+            }
+        }
+
+        public int velkaMatematika(int n, int k)
+        {
+            int[] maxiky = new int[n];
+
+            for (int i = 0; i <= n; i++)
+            {
+                maxiky[i] = Math.Max(tabulka[n-i,k], tabulka[i,k-1]);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
